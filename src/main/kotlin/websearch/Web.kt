9c1151b3c6925b6b3fc255d3e355f2cbe5310377
DeeproChoudhury <72229274+DeeproChoudhury@users.noreply.github.com>
2020-12-04 11:49:43 +0000

    Search Engine and Extensions completed!

--- src/main/kotlin/websearch/Web.kt
+++ src/main/kotlin/websearch/Web.kt
@@ -1,16 +1,33 @@
 package websearch
+import org.jsoup.HttpStatusException
 import org.jsoup.nodes.Document
+import org.jsoup.Jsoup.connect
 
 data class URL (val url: String) {
     override fun toString(): String = url
 
     fun download():WebPage {
+            val downloaded = connect(url).get()
+            return WebPage(downloaded)
 
     }
+
 }
 
 class WebPage (private val webContent: Document) {
     fun extractWords(): List<String> {
         return webContent.text().split(" ").map{s-> s.replace(".", "").replace(",", "")}. map {it.toLowerCase()}
+    }
+
+    fun extractLinks(): List<URL> {
+        val aTags = webContent.getElementsByTag("a")
+        var link = mutableListOf<URL>()
+        for (aTag in aTags) {
+            var urlString = aTag.attr("href")
+            if (urlString.startsWith("https://") || urlString.startsWith("http://")) {
+                link.add(URL(urlString))
+            }
+        }
+        return link
     }
 }
---
+++ src/main/kotlin/websearch/WebCrawler.kt
@@ -0,0 +1,41 @@
+package websearch
+
+class WebCrawler(val url: URL) {
+    private val maxPages = 10
+    fun run() {
+        runhelper(url)
+    }
+
+
+        var storedResults = mutableMapOf<URL, WebPage>()
+        fun runhelper(url: URL) {
+            try {
+                if (storedResults.size < maxPages) {
+                    val downloaded = url.download()
+                    storedResults[url] = downloaded
+                    val extractedLinks = downloaded.extractLinks()
+                    for (extractedLink in extractedLinks) {
+                        if (!storedResults.containsKey(extractedLink)) {
+                            runhelper(extractedLink)
+                        }
+                    }
+                }
+            } catch (e: org.jsoup.HttpStatusException) {
+                println("Sorry, there was an error connecting to the webpage!")
+            }
+
+        }
+
+        fun dump(): MutableMap<URL, WebPage> {
+            return storedResults
+        }
+
+}
+
+fun main() {
+    val crawler = WebCrawler(url = URL("http://www.bbc.co.uk"))
+    crawler.run()
+    val searchEngine = SearchEngine(crawler.dump())
+    searchEngine.compileIndex()
+    println(searchEngine.searchFor("news"))
+}
